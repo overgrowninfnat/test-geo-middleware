6b41270b9aa4cc00e115fdc3090c611402e0b9d6
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-22 15:00:52 +0100

    451 status code added

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,13 +1,12 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-// Block Austria, prefer Germany
 const BLOCKED_COUNTRY = 'IT'
 
 export function middleware(req: NextRequest) {
   const country = req.geo?.country || 'US'
 
   if (country === BLOCKED_COUNTRY) {
-    return NextResponse.redirect('/not-legal')
+    return NextResponse.redirect('/not-legal', 451)
   }
   return NextResponse.next()
 }
