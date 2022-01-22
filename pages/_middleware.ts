20eb353f403154841bd093d3457c053028950b3f
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-22 13:07:47 +0100

    return country from middleware

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,5 +2,5 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
     const country = req.geo?.country || null
-    return new Response('I am coming from and after the middleware')
+    return new Response(country)
 }
