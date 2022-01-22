db2d9c4804ecab4498b7dc540c9e8df031ec7d0e
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-22 14:53:48 +0100

    not legal page

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -7,7 +7,7 @@ export function middleware(req: NextRequest) {
   const country = req.geo?.country || 'US'
 
   if (country === BLOCKED_COUNTRY) {
-    return new Response('Blocked for legal reasons', { status: 451 })
+    return NextResponse.redirect('/not-legal')
   }
   return NextResponse.next()
 }
---
+++ pages/not-legal.tsx
@@ -0,0 +1,7 @@
+export default function NotLegalPage() {
+    return(
+        <div>
+            <h1>Not legal in Italy</h1>
+        </div>
+    )
+}
