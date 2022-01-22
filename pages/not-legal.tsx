38190a0bb0242e94fa47c9de985255793ca1e679
theItalianDev <theItalianDev@MinidiGiuseppe.wind3.hub>
2022-01-22 15:37:52 +0100

    final commit

--- pages/not-legal.tsx
+++ pages/not-legal.tsx
@@ -1,7 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
 export default function NotLegalPage() {
     return(
-        <div>
-            <h1>Not legal in Italy</h1>
-        </div>
+        <div className="w-screen h-screen bg-purple-400 flex flex-col justify-center items-center">
+      <Head>
+        <title>Not legal in Italy</title>
+      </Head>
+      <h1 className='text-4xl text-white mb-10'>NOT LEGAL IN ITALY</h1>
+      <Image
+        src="/notpineapplepizza.png"
+        width={400}
+        height={400}
+        layout='fixed'
+        className='rounded-xl'
+      />
+    </div>
     )
 }
