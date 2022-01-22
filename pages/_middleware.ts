27bb6e885d17347f1ff97bde44050ca7b2d7d7aa
theItalianDev <theItalianDev@Mac-mini-di-Giuseppe.local>
2022-01-22 14:39:43 +0100

    geo blocking

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,13 @@
-import type { NextRequest } from "next/server";
+import type { NextRequest } from 'next/server'
 
-export async function middleware(req: NextRequest) {
-    const country = req.geo?.country || null
-    return new Response(country)
+// Block Austria, prefer Germany
+const BLOCKED_COUNTRY = 'IT'
+
+export function middleware(req: NextRequest) {
+  const country = req.geo?.country || 'US'
+
+  if (country === BLOCKED_COUNTRY) {
+    return new Response('Blocked for legal reasons', { status: 451 })
+  }
+  return new Response(`Greetings from ${country}, where you are not blocked.`)
 }
